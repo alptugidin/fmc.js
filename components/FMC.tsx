e43780a4f7c50b8e32e919f37a70c9fdff5ff1f9
Alptuğ <alptugidin@gmail.com>
2023-04-03 00:04:06 +0300

    add provider

--- components/FMC.tsx
+++ components/FMC.tsx
@@ -1,22 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 import { View, Text, Image } from 'react-native';
 import Screen from './Screen/Screen';
 import PageButtons from './PageButtons/PageButtons';
 import Numpad from './Numpad/Numpad';
 import Keypad from './Keypad/Keypad';
 import { init } from '../socket';
+interface IFmcContext {
+    socket: any
+}
+interface ProviderProps {
+    children: React.ReactNode
+}
+const FmcContext = createContext<IFmcContext>({} as IFmcContext);
+export const useFmc = (): any => useContext(FmcContext);
+
 const FMC: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
   const { socket } = init();
+
+  const FmcProvider: React.FC<ProviderProps> = ({ children }) => {
+    return (
+      <FmcContext.Provider value={socket}>
+        {children}
+      </FmcContext.Provider>
+    );
+  };
 
   return (
     <View className={'bg-[#565656] flex-1'}>
-      <Screen socket={socket}/>
-      <PageButtons socket={socket}/>
-      <View className={'flex flex-row'}>
-        <Numpad/>
-        <Keypad/>
-      </View>
+      <FmcProvider>
+        <Screen/>
+        <PageButtons/>
+        <View className={'flex flex-row'}>
+          <Numpad/>
+          <Keypad/>
+        </View>
+      </FmcProvider>
     </View>
   );
 };
