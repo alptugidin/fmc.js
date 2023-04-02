f8f5328471399678c06b0c88f5201473f895bbdc
Alptuğ <alptugidin@gmail.com>
2023-04-03 00:03:00 +0300

    testing lvar emit

--- components/Keypad/Button.tsx
+++ components/Keypad/Button.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { useFmc } from '../FMC';
 interface IButton {
     label: string
     disableMl?: boolean
 }
 const Button: React.FC<IButton> = ({ label, disableMl }) => {
+  const socket = useFmc();
+  const handleKeyDown = (): void => {
+    socket.emit('write', 'L:switch_573_73X');
+  };
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={handleKeyDown}>
       <View className={'w-[16%] h-[40px] rounded-lg bg-black flex justify-center items-center mt-[12px]'}>
         <Text className={'color-white text-xl'}>
           {label}
